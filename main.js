55c4a7dbbd768f95cb3b013a104d0b87b95e1142
Nikhil Kurup <84773412+nikhil937@users.noreply.github.com>
2022-05-27 15:28:52 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -6,12 +6,40 @@ function preload(){
     enemy = loadSound("enemy.mp3");
 }
 
+rightWristX = 0;
+rightWristY = 0;
+
+leftWristX = 0;
+leftWristY = 0;
+
 function setup() {
     canvas = createCanvas(600, 500);
     canvas.center();
   
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+  }
+  
+  function modelLoaded() {
+  
+    console.log("poseNet is initialized");
+  }
+
+  function gotPoses(results) {
+    if (results.length > 0) {
+      console.log(results);
+  
+      rightWristX = results[0].pose.rightWrist.x;
+      rightWristY = results[0].pose.rightWrist.y;
+      console.log("rightWristX =" + rightWristX + "rightWristY = " + rightWristY);
+  
+      leftWristX = results[0].pose.leftWrist.x;
+      leftWristY = results[0].pose.leftWrist.y;
+      console.log("leftWristX =" + leftWristX + "leftWristY = " + leftWristY);
+    }
   }
 
   function draw() {
