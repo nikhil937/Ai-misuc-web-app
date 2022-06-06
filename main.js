1858021e7760a3b8d388061d2bcf45a7879161a3
Nikhil Kurup <84773412+nikhil937@users.noreply.github.com>
2022-06-06 16:58:31 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,7 +5,10 @@ rightWrist_y = 0;
 leftWrist_x = 0;
 leftWrist_y = 0;
 scoreleftWrist = 0;
-song_name = "";
+scorerightWrist = 0;
+song_thunder = "";
+song_enemy = "";
+
 
 function setup(){
     canvas = createCanvas(600,530);
@@ -23,24 +26,40 @@ function preload(){
     enemy = loadSound("enemy.mp3");
 }
 
+
 function draw(){
     image(video,0,0,600,530);
 
     fill("#00ff00");
     stroke("#ff0000");
 
-    song_name = thunder.isPlaying();
-    console.log(song_name);
+    song_thunder = thunder.isPlaying();
+    console.log(song_thunder);
+
+    song_thunder = thunder.isPlaying();
+    console.log(song_thunder);
 
     if(scoreleftWrist > 0.2){
         circle(leftWrist_x,leftWrist_y,20);
         enemy.stop();
-        if(song_name == false){
+        if(song_thunder == false){
             thunder.play();
         }
         else{
-            console.log("Song Name: Thunder");
-            document.getElementById("song_id").innerHTML = "Song Name: Thunder";
+            console.log("Song Name: Thunder Song");
+            document.getElementById("song_id").innerHTML = "Song Name: Thunder Song";
+        }
+    }
+
+    if(scorerightWrist > 0.2){
+        circle(rightWrist_x,rightWrist_y,20);
+        thunder.stop();
+        if(song_enemy == false){
+            enemy.play();
+        }
+        else{
+            console.log("Song Name: Enemy Song");
+            document.getElementById("song_id").innerHTML = "Song Name: Enemy Song";
         }
     }
 }
@@ -55,6 +74,9 @@ function gotposes(results){
 
         scoreleftWrist = results[0].pose.keypoints[9].score;
         console.log(scoreleftWrist);
+
+        scorerightWrist = results[0].pose.keypoints[10].score;
+        console.log(scorerightWrist);
 
         leftWrist_x = results[0].pose.leftWrist.x;
         leftWrist_y = results[0].pose.leftWrist.y;
